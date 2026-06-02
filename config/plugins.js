62882874305c42b0f94ecb2942250f2d72282014
Seppe Alaerts <hello@ringtail.dev>
2026-06-02 14:32:32 +0000

    feat: add strapi-plugin-navigation
    Install strapi-plugin-navigation@3.3.7 (Strapi v5 compatible).
    Configure plugin with article content type support using pageTitle
    as display field and slug for path generation. Allowed nesting
    levels set to 2.

--- config/plugins.js
+++ config/plugins.js
@@ -16,4 +16,29 @@ module.exports = ({ env }) => ({
   "google-maps": {
     enabled: true,
   },
+  navigation: {
+    enabled: true,
+    config: {
+      contentTypes: [
+        'api::article.article',
+        'api::manual.manual',
+        'api::group.group',
+        'api::rental-location.rental-location',
+      ],
+      defaultContentType: 'api::article.article',
+      contentTypesNameFields: {
+        'api::article.article': ['pageTitle'],
+        'api::manual.manual': ['title'],
+        'api::group.group': ['pageTitle'],
+        'api::rental-location.rental-location': ['pageTitle'],
+      },
+      pathDefaultFields: {
+        'api::article.article': ['slug'],
+        'api::manual.manual': ['slug'],
+        'api::group.group': ['slug'],
+        'api::rental-location.rental-location': ['slug'],
+      },
+      allowedLevels: 2,
+    },
+  },
 });
