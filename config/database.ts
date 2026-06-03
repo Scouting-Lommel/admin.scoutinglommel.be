fc188ee7a031db9230c3fadc9f2e33bbb0e2b0f8
Seppe Alaerts <hello@ringtail.dev>
2026-06-03 12:29:18 +0000

    refactor(config): migrate config files to TypeScript

---
+++ config/database.ts
@@ -0,0 +1,13 @@
+export default ({ env }) => ({
+  connection: {
+    client: "mysql",
+    connection: {
+      host: env("DB_HOST"),
+      port: env.int("DB_PORT"),
+      database: env("DB_NAME"),
+      user: env("DB_USERNAME"),
+      password: env("DB_PASSWORD"),
+    },
+    debug: false,
+  },
+});
